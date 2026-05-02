299269c3cef8f6f12f65e521484b48245863e77b
DawidMazurek4 <dawid5mazurek@gmail.com>
2026-05-02 14:39:58 +0200

    get /game function

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,9 @@
 import { get } from "http";
 
-export const dbUrl = process.env.BACKEND_URL;
-
+// export const dbUrl = process.env.BACKEND_URL;
+export const dbUrl = "http://localhost:8080";
+export var gameid: number = 0;
+export var sessionKey: string = "";
 export async function getLanguages(){
     var all_languages: string[] = []
     const res = await fetch(`${dbUrl}/languages`);
@@ -10,3 +12,16 @@ export async function getLanguages(){
     console.log(all_languages);
     return all_languages;
 }
+
+export async function newGame(){
+    const res = await fetch(`${dbUrl}/game`);
+    const data = await res.json();
+    data.game_id = parseInt(data.game_id);
+    gameid = data.game_id;
+    sessionKey = data.session_key;
+    return data;
+}
+
+newGame().then(() => {
+    console.log(`Game ID: ${gameid}, Session Key: ${sessionKey}`);
+});
